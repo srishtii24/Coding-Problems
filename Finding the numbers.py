9daa78e5fc89981b9417ce87b285271dcf28c6fe
Srishti Gupta <47052945+srishtii24@users.noreply.github.com>
2020-07-25 22:42:03 +0530

    Add files via upload

---
+++ Finding the numbers.py
@@ -0,0 +1,44 @@
+"""
+You are given an array A containing 2*N+2 positive numbers, out of which 2*N numbers exist in pairs whereas the other two number occur exactly once and are distinct. You need to find the other two numbers and print them in ascending order.
+ 
+
+Input :
+The first line contains a value T, which denotes the number of test cases. Then T test cases follow .The first line of each test case contains a value N. The next line contains 2*N+2 space separated integers.
+
+Output :
+Print in a new line the two numbers in ascending order.
+
+Constraints :
+1<=T<=100
+1<=N<=10^6
+1<=A[i]<=5*10^8
+
+Example:
+Input :
+2
+2
+1 2 3 2 1 4
+1
+2 1 3 2
+
+Output :
+3 4
+1 3
+"""
+
+
+#code
+t = int(input())
+for testcase in range(t):
+    num = int(input())
+    arr = list(map(int, input().split()))
+    pairs = {}
+    for i in arr:
+        if i not in pairs:
+            pairs[i] = 1
+        else:
+            del pairs[i]
+    num = sorted([n for n in pairs.keys()])
+    print(num[0], num[1])
+    
+    
